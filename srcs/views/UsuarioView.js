0ea7c5fec333da3756b1cf9efe0d2f0e4c6cd27b
Fabinnreis <fabioreisti@outlook.com>
2020-07-20 12:04:32 -0300

    adicionado endereco model e view, alterado view usuario

--- srcs/views/UsuarioView.js
+++ srcs/views/UsuarioView.js
@@ -49,35 +49,6 @@ class UsuarioView{
             <input type="password" class="form-control" id="retypePassword" placeholder="Password">
             </div>
             </div>
-            <!--Informações de endereço-->
-            <div id="infoEndereco">
-            <!--Campo de endereço-->
-            <div class="form-group">
-            <label for="inputCep">Cep</label>
-            <input type="text" class="form-control" id="inputCep" placeholder="Cep">
-            </div><br><br>
-            <div class="form-group">
-            <label for="inputLogradouro">Logradouro</label>
-            <input type="text" class="form-control" id="inputLogradouro" placeholder="Logradouro">
-            </div>
-            <div class="form-group">
-            <label for="inputComplemento">Complemento</label>
-            <input type="text" class="form-control" id="inputComplemento" placeholder="Complemento">
-            </div>
-            <div class="form-group">
-            <label for="inputBairro">Bairro</label>
-            <input type="text" class="form-control" id="inputBairro" placeholder="Bairro">
-            </div>
-            <div class="form-group">
-            <label for="inputLocalidade">Localidade</label>
-            <input type="text" class="form-control" id="inputLocalidade" placeholder="Localidade">
-            </div>
-            <div class="form-group">
-            <label for="inputUf">Uf</label>
-            <input type="text" class="form-control" id="inputUf" placeholder="Uf">
-            </div>
-            </div>
-
             <!--Botão de envio-->
             <div id ="btnCadastroForm"><button type="submit" class="btn btn-primary">Submit</button></div>
             </form>
