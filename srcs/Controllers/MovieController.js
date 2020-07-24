915aeb82cdf984d1f55e2a639b5ca4620421a358
Gabrielle Sant'Anna <g.santanna21@hotmail.com>
2020-07-23 22:01:31 -0300

    merge

---
+++ srcs/Controllers/MovieController.js
@@ -0,0 +1,24 @@
+class MovieController {
+
+    static generateMovie(movieUrl) {
+        let printView = document.querySelector("#infoMovie")
+        let request = new XMLHttpRequest();
+        request.open("GET", movieUrl);
+        request.addEventListener("load",() => {
+            let reqObj = JSON.parse (request.responseText);
+            let sendMovie = new Movie(reqObj.Poster,
+                                      reqObj.Title,
+                                      reqObj.Year,
+                                      reqObj.Runtime,
+                                      reqObj.Genre,
+                                      reqObj.reqObjDirector,
+                                      reqObj.Actors,
+                                      reqObj.Plot,
+                                      reqObj.imdbRating,
+                                      reqObj.imdbVotes,
+                                      reqObj.Awards);
+        printView.innerHTML = MovieView.templateMovie(sendMovie.getMovie());
+        })
+        request.send()
+    }
+}
