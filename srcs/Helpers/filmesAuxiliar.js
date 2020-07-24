45e81af68fb94c4cb45425effac25fe9d96e96e2
Samantha Cardoso <sassacardosoo@gmail.com>
2020-07-23 21:57:58 -0300

    adição de nova pagina

--- srcs/Helpers/filmesAuxiliar.js
+++ srcs/Helpers/filmesAuxiliar.js
@@ -4,7 +4,7 @@ movieString = queryString;
 movieString = movieString.split("_").join(" ");
 
 let movieUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=982c7993&t=${movieString}`;
-let trailerUrl = `https://www.googleapis.com/youtube/v3/search?q="${movieString} trailer"&key=${googleApiKey}`
+let trailerUrl = `https://www.googleapis.com/youtube/v3/search?q="${movieString} trailer"&key=AIzaSyDQSZMNFwD26NHsNiejiIbjjEJsS0GkDv8S`
 
 MovieController.generateMovie(movieUrl);
 TrailerController.generateTrailer(trailerUrl);
