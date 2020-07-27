120d0d299f4310c2f952487db50085afac178bad
Ivan <ivanvalverde53@hotmail.com>
2020-07-27 02:27:29 -0300

    Correções

--- srcs/Helpers/filmesAuxiliar.js
+++ srcs/Helpers/filmesAuxiliar.js
@@ -5,7 +5,7 @@ movieString = movieString.split("_").join(" ");
 let errorReq = false;
 let movieRegExp = /filmes/;
 
-let movieUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=982c7993&t=${movieString}`;
+let movieUrl = `https://www.omdbapi.com/?i=tt3896198&apikey=982c7993&t=${movieString}`;
 let trailerUrl = `https://www.googleapis.com/youtube/v3/search?q="${movieString} trailer"&key=AIzaSyCRTOBtfGl9kRAnM4QLJ4pcF00Y1Bv9YfY`
 
 MovieController.generateMovie(movieUrl);
--- srcs/Helpers/searchPageHelper.js
+++ srcs/Helpers/searchPageHelper.js
@@ -6,7 +6,7 @@ let errorReq = false;
 
 
 btnSearch.addEventListener("click", ()=>{
-    let movieUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=982c7993&t=${movieName.value}`;
+    let movieUrl = `https://www.omdbapi.com/?i=tt3896198&apikey=982c7993&t=${movieName.value}`;
     let trailerUrl = `https://www.googleapis.com/youtube/v3/search?q="${movieName.value} trailer"&key=AIzaSyCRTOBtfGl9kRAnM4QLJ4pcF00Y1Bv9YfY`
 
     MovieController.generateMovie(movieUrl);
