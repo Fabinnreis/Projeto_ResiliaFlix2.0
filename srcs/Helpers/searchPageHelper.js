01960664a3accc92284578b4c7669535dae15554
Gabrielle Sant'Anna <g.santanna21@hotmail.com>
2020-07-26 23:29:54 -0300

    alteração da responsividade da navbar

--- srcs/Helpers/searchPageHelper.js
+++ srcs/Helpers/searchPageHelper.js
@@ -7,7 +7,7 @@ let errorReq = false;
 
 btnSearch.addEventListener("click", ()=>{
     let movieUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=982c7993&t=${movieName.value}`;
-    let trailerUrl = `https://www.googleapis.com/youtube/v3/search?q="${movieName.value} trailer"&key=${googleApiKey}`
+    let trailerUrl = `https://www.googleapis.com/youtube/v3/search?q="${movieName.value} trailer"${googleApiKey}`
 
     MovieController.generateMovie(movieUrl);
     TrailerController.generateTrailer(trailerUrl);
